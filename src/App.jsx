10d363cc59d14342195240929efb367bd0914f32
Mighty Bajnr <mightybajnr@gmail.com>
2025-08-13 08:32:02 +0000

    me

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage"
+import LandingPage from "./pages/LandingPage";
 import EcommercePage from "./pages/EcommercePage";
 import Dashboard from "./components/Admin/Dashboard";
-
+import ProductDetails from "./components/Ecommerce/ProductDetails";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/shopPage" element={<EcommercePage />} />
+        <Route path="/shopPage:id" element={<ProductDetails />} />
       </Routes>
     </>
   );
---
+++ src/components/Ecommerce/ProductDetails.jsx
@@ -0,0 +1,10 @@
+function ProductDetails() {
+  return (
+    <div>
+      <h1>Product Details</h1>
+        <p>Here you can find detailed information about the product.</p>
+
+    </div>
+  );
+}
+export default ProductDetails
